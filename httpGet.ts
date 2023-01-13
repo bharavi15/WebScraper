5678bbc2a34f287a93d5cd5ccc8ad54f959fadfa
Bharavi Lakhote <bharavi15@gmail.com>
2023-01-13 22:32:14 +0530

    linting

--- httpGet.ts
+++ httpGet.ts
@@ -1,12 +1,21 @@
 import axios, { AxiosInstance } from 'axios'
+import logger from './logger'
+const LOG_PREFIX = 'httpGet |'
+const MAX_DEPTH = parseInt(process.env.MAX_DEPTH ?? '5')
 export function createHttpHandler (baseUrl: string) {
+  logger.info(`${LOG_PREFIX} creating http handler for ${baseUrl}`)
   return axios.create({
     baseURL: baseUrl,
     timeout: 10000,
     headers: {}
   })
 }
-export async function get (instance: AxiosInstance, path: string) {
+export async function get (instance: AxiosInstance, path: string, depth = 0): Promise<string> {
+  logger.info(`${LOG_PREFIX} getting path ${path} at depth ${depth}`)
+
+  if (depth > MAX_DEPTH) {
+    logger.info(`${LOG_PREFIX} ignoring path ${path} as depth > ${MAX_DEPTH}`)
+  }
   return await instance.get(path).then(async (response: any) => {
     return await new Promise((resolve, reject) => {
       if (response.status !== 200) {
--- index.ts
+++ index.ts
@@ -1,7 +1,48 @@
 import { createHttpHandler, get } from './httpGet'
 import { writeFileSync } from 'fs'
 import logger from './logger'
-const LOG_PREFIX = 'index | '
+const LOG_PREFIX = 'index |'
+const toBeVisitedUrls: string[] = []
+const visitedUrls: string[] = []
+
+logger.info(`${LOG_PREFIX} Hello world`)
+const fqdn = 'google.co.in'
+const host = 'https://' + fqdn
+const axiosInstance = createHttpHandler(host)
+toBeVisitedUrls.push(host)
+get(axiosInstance, '/').then((response: any) => {
+  const urls = response.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm)
+  const filteredUrls = urls
+    .filter(filterStaticFiles)
+    .filter(filterThirdPartyHosts)
+  visitedUrls.push(host)
+  toBeVisitedUrls.push(...filteredUrls)
+  // const chunkedUrls = chunkify(filteredUrls, 5)
+  // writeFileSync('urls.out.json', JSON.stringify(chunkedUrls, null, 2))
+  logger.debug(`${LOG_PREFIX} unvisitedUrls are ${JSON.stringify(toBeVisitedUrls)}`)
+  return filteredUrls
+}).then((chunkedUrls) => {
+})
+async function visitNode (path: string): Promise<string[]> {
+  const response = await get(axiosInstance, '/')
+  const urls = response.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm) ?? []
+  const filteredUrls = urls
+    .filter(filterStaticFiles)
+    .filter(filterThirdPartyHosts)
+    .filter(filterVisitedUrls)
+  visitedUrls.push(host)
+  toBeVisitedUrls.push(...filteredUrls)
+  return []
+}
+function filterStaticFiles (url: string) {
+  return url.match(/\.(js|pdf|docx?|css|map|jpe?g|png|svg)/gm)
+}
+function filterThirdPartyHosts (url: string) {
+  return url.includes(fqdn)
+}
+function filterVisitedUrls (url: string) {
+  return visitedUrls.includes(url)
+}
 function chunkify (arr: string[], chunkSize: number): string[][] {
   const result: string[][] = []
   for (let i = 0; i < arr.length; i += chunkSize) {
@@ -13,25 +54,4 @@ function chunkify (arr: string[], chunkSize: number): string[][] {
     result.push(result1)
   }
   return result
-}
-logger.info(LOG_PREFIX+ 'Hello world')
-const hostname = 'google.co.in'
-const axiosInstance = createHttpHandler('https://' + hostname)
-get(axiosInstance, '').then((response: any) => {
-  const urls = response.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gm)
-  const filteredUrls = urls
-    .filter(filterStaticFiles)
-    .filter(filterThirdPartyHosts)
-  const chunkedUrls = chunkify(filteredUrls, 5)
-  writeFileSync('urls.out.json', JSON.stringify(chunkedUrls, null, 2))
-  return chunkedUrls
-}).then((chunkedUrls) => {
-
-})
-
-function filterStaticFiles (url: string) {
-  return url.match(/\.(js|pdf|docx?|css|map|jpe?g|png|svg)/gm) == null
-}
-function filterThirdPartyHosts (url: string) {
-  return url.includes(hostname)
 }
